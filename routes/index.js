e833f0b9733f075ca2f2b94940df6764ad19be0b
Jason446620 <panshubei@outlook.com>
2020-07-09 14:23:47 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const mysql = require('mysql2');
 const fs = require('fs');
 const path = require('path');
 let result = "";
-let re = "";
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
