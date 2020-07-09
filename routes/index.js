be86ffdf440c168b871558dceea4ef367aa446da
Jason446620 <panshubei@outlook.com>
2020-07-09 14:10:41 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+const mysql = require('mysql2');
+const fs = require('fs');
+const path = require('path');
+let result = "";
+let re = "";
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var connection = mysql.createConnection({
+    host: 'panshubeimysql.mysql.database.azure.com',
+    port: '3306',
+    user: 'panshubei@panshubeimysql',
+    password: 'Jason.446620',
+    database: 'mysql',
+    ssl: {
+        ca: fs.readFileSync(path.resolve(__dirname, 'BaltimoreCyberTrustRoot.crt.pem'))
+    }
+});
+
+connection.connect();
+
+connection.query('SELECT *FROM USER', function (error, results) {
+    if (error) throw error;
+    for (i = 0;i < results.length; i++) { 
+        console.log('The user is: '+results[i].User);
+        result += results[i].User+", ";
+        console.log('The user is: '+result);
+    }
+    
+});
+  connection.end();
+  res.render('index', { title: result });
+});
+
+module.exports = router;
